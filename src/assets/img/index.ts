c16080e0bceb905248000eb30f6223a105e21cf5
natabgaf <natanfernandes52@gmail.com>
2024-02-01 13:22:55 -0300

    Feature :update Page about

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -2,7 +2,7 @@ import logo from "./logo.png";
 import miniLogo from "./miniLogo.png";
 import PrincipalImage from "./PrincipalImage.jpg";
 import sentinela from "./sentinela.jpg";
-import about from "./about.png";
+import about from "./about.jpg";
 import aboutWatch from "./aboutWatch.png";
 
 export { logo ,miniLogo  ,PrincipalImage ,sentinela ,about,aboutWatch};
